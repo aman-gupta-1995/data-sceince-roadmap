6b58edd07651b2ed95f860ff20e5e8742c4ca054
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-05 16:33:15 +0530

    Update 2_descriptive-statistics.py

--- data-scientist-roadmap-master/02_Statistics/2_descriptive-statistics.py
+++ data-scientist-roadmap-master/02_Statistics/2_descriptive-statistics.py
@@ -6,7 +6,7 @@ import numpy as np
 # Create a dataset    
 dataset = [12, 52, 45, 65, 78, 11, 12, 54, 56]   
 
-# Apply mean/median functions
+# Apply mean/median functions 
 dataset_mean = np.mean(dataset) 
 dataset_median = np.median(dataset)
 
