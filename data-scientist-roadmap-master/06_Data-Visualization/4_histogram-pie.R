2641906947c0fcd21704b72febb4874df0f1f0a4
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-04 21:38:29 +0530

    Update 4_histogram-pie.R

--- data-scientist-roadmap-master/06_Data-Visualization/4_histogram-pie.R
+++ data-scientist-roadmap-master/06_Data-Visualization/4_histogram-pie.R
@@ -1,7 +1,7 @@
 # Two plot on the same window 
 par(mfrow = c(1,2))   
 # Histogram   
-data <- iris 
+data <- iris
 hist(data[,2], main = "histogram about sepal width", xlab = "sepal width", ylab = "Frequency")
 # Pie chart
 classes <- summary(data[,5])
